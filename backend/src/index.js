ef99b299c3f6b0ecb292184f6faa6d355976c42b
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2026-02-10 09:06:55 +0530

    feat(server): setup express server with database connection and error handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || PORT, () => {
+      console.log(`Server is running on PORT :: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Server connection error !!! ${err}`);
+  });
