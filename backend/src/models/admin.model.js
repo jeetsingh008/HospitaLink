76e25f978a69154afdb3b19eef7447e34ed8a1a8
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2026-02-10 11:13:19 +0530

    feat(db): add admin model with name, username, password, and role fields

---
+++ backend/src/models/admin.model.js
@@ -0,0 +1,29 @@
+import { mongoose, Schema } from "mongoose";
+
+const adminSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    role: {
+      type: String,
+      default: "admin",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Admin = mongoose.model("Admin", adminSchema);
